32cf831325fe277639dcbd12d9c1ccab58e6bb12
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2017-08-30 07:03:00 +0200

    use provided uri->key function to assign resources to correct pool

--- src/severin/core.clj
+++ src/severin/core.clj
@@ -14,26 +14,30 @@
   (-uri->key
     [this uri]))
 
+(defn- uri->key
+  [f uri]
+  (if (satisfies? URI->KeyProtocol f)
+    (-uri->key f uri)
+    (keyword uri)))
+
 (defmulti factory
   #(-> %
        java.net.URI.
        .getScheme))
 
 (defn- pool-pop!
-  [pool-ref uri]
-  (let [k (keyword uri)]
-    (dosync
-      (when-let [v (peek (k @pool-ref))]
-        (alter pool-ref update-in [k] pop)
-        v))))
+  [pool-ref k]
+  (dosync
+    (when-let [v (peek (k @pool-ref))]
+      (alter pool-ref update-in [k] pop)
+      v)))
 
 (defn- pool-push!
-  [pool-ref uri resource max-size]
-  (let [k (keyword uri)]
-    (dosync
-      (if (> max-size (count (k @pool-ref)))
-        (some? (alter pool-ref update-in [(keyword uri)] conj resource))
-        false))))
+  [pool-ref k resource max-size]
+  (dosync
+    (if (> max-size (count (k @pool-ref)))
+      (some? (alter pool-ref update-in [k] conj resource))
+     false)))
 
 (defn make-pool
   [&{:keys [max-size] :or {max-size 10}}]
@@ -43,7 +47,7 @@
 (defn create!
   [pool-ref uri]
   (let [f (factory uri)]
-    (if-let [r (pool-pop! pool-ref uri)]
+    (if-let [r (pool-pop! pool-ref (uri->key f uri))]
       (if-not (-valid? f r)
         (do
           (-dispose! f r)
@@ -55,7 +59,7 @@
   ([pool-ref uri resource]
    (let [f (factory uri)]
      (if-not (and (-valid? f resource)
-                  (pool-push! pool-ref uri resource (:max-size (meta pool-ref))))
+                  (pool-push! pool-ref (uri->key f uri) resource (:max-size (meta pool-ref))))
        (-dispose! f resource))))
   ([pool-ref resource]
     (if-let [uri (:uri resource)]
